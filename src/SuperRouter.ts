3819cfcc2bb2460bd12bb795ccb9c21836d0154e
Rishabh Mishra <me@rishabhmishra.dev>
2019-09-08 12:25:50 +0530

    update packages

--- src/SuperRouter.ts
+++ src/SuperRouter.ts
@@ -1,19 +1,36 @@
-import { app, RegisterStreamProtocolRequest, remote, session, StreamProtocolResponse, UploadData } from 'electron';
-import queryString from 'query-string';
+import {
+  app,
+  RegisterStreamProtocolRequest,
+  remote,
+  session,
+  StreamProtocolResponse,
+  UploadData
+} from "electron";
+import queryString from "query-string";
 
-import { ElectronResponse } from './Response';
-import { Router } from './Router';
-import { ElectronRequest, EnhancedUploadData, RequestHandler, AnyJson } from './types';
+import { ElectronResponse } from "./Response";
+import { Router } from "./Router";
+import {
+  ElectronRequest,
+  EnhancedUploadData,
+  RequestHandler,
+  AnyJson
+} from "./types";
 
 const electronApp = app || remote.app;
 const electronSession = session || remote.session;
 
 function enhanceUploadData(uploadData: UploadData[]): EnhancedUploadData[] {
   return uploadData.map((data: UploadData) => {
-    const enhancedData: EnhancedUploadData = {...data, stringContent: null, json: null}
+    const enhancedData: EnhancedUploadData = {
+      ...data,
+      stringContent: null,
+      json: null
+    };
     if (data.bytes && data.bytes.toString) {
       enhancedData.stringContent = (): string => data.bytes.toString();
-      enhancedData.json = (): AnyJson => enhancedData.stringContent && JSON.parse(enhancedData.stringContent());
+      enhancedData.json = (): AnyJson =>
+        enhancedData.stringContent && JSON.parse(enhancedData.stringContent());
     }
     return enhancedData;
   });
@@ -27,20 +44,20 @@ export class SuperRouter extends Router {
     if (electronApp.isReady()) {
       this.registerScheme();
     } else {
-      electronApp.on('ready', this.registerScheme.bind(this));
+      electronApp.on("ready", this.registerScheme.bind(this));
     }
   }
 
   private registerScheme(): void {
     if (electronSession.defaultSession) {
-    electronSession.defaultSession.protocol.unregisterProtocol(this.scheme);
-    electronSession.defaultSession.protocol.registerStreamProtocol(
-      this.scheme,
-      this._handle.bind(this),
-      (error: Error) => {
-        if (error) throw error;
-      }
-    );
+      electronSession.defaultSession.protocol.unregisterProtocol(this.scheme);
+      electronSession.defaultSession.protocol.registerStreamProtocol(
+        this.scheme,
+        this._handle.bind(this),
+        (error: Error) => {
+          if (error) throw error;
+        }
+      );
     }
   }
 
@@ -48,7 +65,10 @@ export class SuperRouter extends Router {
     this._methods.use.splice(0, this._methods.use.length);
   }
 
-  private _handle(request: RegisterStreamProtocolRequest, cb: (response?: StreamProtocolResponse) => void): void {
+  private _handle(
+    request: RegisterStreamProtocolRequest,
+    cb: (response?: StreamProtocolResponse) => void
+  ): void {
     const { url, referrer, method, uploadData, headers } = request;
     const path = decodeURIComponent(new URL(url).pathname);
     const query = queryString.parse(decodeURIComponent(new URL(url).search));
@@ -67,10 +87,13 @@ export class SuperRouter extends Router {
         method,
         referrer,
         uploadData: uploadData_,
-        body: uploadData_.length && uploadData_[0].json ? uploadData_[0].json() : {},
+        body:
+          uploadData_.length && uploadData_[0].json
+            ? uploadData_[0].json()
+            : {},
         url: path,
         headers,
-        query,
+        query
       };
 
       const attemptHandler = (index: number): void => {
@@ -79,7 +102,9 @@ export class SuperRouter extends Router {
 
         const next = (): void => {
           if (res.called) {
-            throw new Error("Can't call next once data has already been sent as a response");
+            throw new Error(
+              "Can't call next once data has already been sent as a response"
+            );
           }
 
           if (index + 1 < handlers.length) {
